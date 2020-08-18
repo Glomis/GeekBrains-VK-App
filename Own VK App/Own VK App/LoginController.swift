951519d759e50194b8d83f0e173da9fb09564d7b
Maxim <55534590+Glomis@users.noreply.github.com>
2020-08-18 22:23:51 +0300

    finished

--- Own VK App/Own VK App/LoginController.swift
+++ Own VK App/Own VK App/LoginController.swift
@@ -2,7 +2,7 @@
 //  ViewController.swift
 //  Own VK App
 //
-//  Created by Max on 16.08.2020.
+//  Created by Tatyana on 18.08.2020.
 //  Copyright © 2020 Max&Co. All rights reserved.
 //
 
@@ -10,48 +10,49 @@ import UIKit
 
 class LoginController: UIViewController {
     
+    
     //Outlets
-    @IBOutlet weak var enterBtn: UIButton!
-    @IBOutlet weak var loginTF: UITextField!
-    @IBOutlet weak var passwordTF: UITextField!
-    @IBOutlet weak var scrollView: UIScrollView!
-    
-    //Variables
-    let userLogin = "admin"
-    let userPassword = "admin"
-    
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-        enterBtn.layer.cornerRadius = 12
-        
-        NotificationCenter.default.addObserver(self, selector: #selector(kbWillShow), name: UIResponder.keyboardWillShowNotification, object: nil)
-    }
-    
-    @objc func kbWillShow(notificaion: Notification) {
-        guard let keyboardRect = notificaion.userInfo?[UIResponder.keyboardFrameBeginUserInfoKey] as? CGRect
-        else { return }
-        
-        scrollView.contentInset.bottom = keyboardRect.height
-    }
-    
-    func showAlert() {
-        let alert = UIAlertController(title: "Ошибка",
-                                      message: "Проверьте правильность введенных данных.",
-                                      preferredStyle: .alert)
-        let okAction = UIAlertAction(title: "Ок", style: .cancel)
-        
-        alert.addAction(okAction)
-        present(alert, animated: true)
-    }
-    
-    
-    //MARK: IBActions
-    @IBAction func enterBtnPressed(_ sender: Any) {
-        if loginTF.text != userLogin || passwordTF.text != userPassword {
-            showAlert()
-        }
-    }
+       @IBOutlet weak var enterBtn: UIButton!
+       @IBOutlet weak var loginTF: UITextField!
+       @IBOutlet weak var passwordTF: UITextField!
+       @IBOutlet weak var scrollView: UIScrollView!
+       
+       //Variables
+       let userLogin = "admin"
+       let userPassword = "admin"
+       
+       
+       override func viewDidLoad() {
+           super.viewDidLoad()
+           
+           enterBtn.layer.cornerRadius = 12
+           
+           NotificationCenter.default.addObserver(self, selector: #selector(kbWillShow), name: UIResponder.keyboardWillShowNotification, object: nil)
+       }
+       
+       @objc func kbWillShow(notificaion: Notification) {
+           guard let keyboardRect = notificaion.userInfo?[UIResponder.keyboardFrameBeginUserInfoKey] as? CGRect
+           else { return }
+           
+           scrollView.contentInset.bottom = keyboardRect.height
+       }
+       
+       func showAlert() {
+           let alert = UIAlertController(title: "Ошибка",
+                                         message: "Проверьте правильность введенных данных.",
+                                         preferredStyle: .alert)
+           let okAction = UIAlertAction(title: "Ок", style: .cancel)
+           
+           alert.addAction(okAction)
+           present(alert, animated: true)
+       }
+       
+       
+       //MARK: IBActions
+       @IBAction func enterBtnPressed(_ sender: Any) {
+           if loginTF.text != userLogin || passwordTF.text != userPassword {
+               showAlert()
+           }
+       }
 }
 
