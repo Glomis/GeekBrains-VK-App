33b581011c31b2b9ddea943cb8008dab13bdc2a7
Maxim <55534590+Glomis@users.noreply.github.com>
2020-08-19 22:53:51 +0300

    Add model and controllers

---
+++ Own VK App/Own VK App/Model/Group.swift
@@ -0,0 +1,10 @@
+//
+//  Group.swift
+//  Own VK App
+//
+//  Created by Tatyana on 19.08.2020.
+//  Copyright © 2020 Max&Co. All rights reserved.
+//
+
+import UIKit
+
---
+++ Own VK App/Own VK App/Model/User.swift
@@ -0,0 +1,9 @@
+//
+//  User.swift
+//  Own VK App
+//
+//  Created by Tatyana on 19.08.2020.
+//  Copyright © 2020 Max&Co. All rights reserved.
+//
+
+import UIKit
